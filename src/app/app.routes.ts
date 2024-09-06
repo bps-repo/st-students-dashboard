e8cae0f745f8512fa7a342d85878fa8100dab5a9
heldersantiago273 <heldersantiago273@gmail.com>
2024-09-06 18:45:40 +0100

    :sparkles: add profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,9 @@ export const routes: Routes = [
   {
     path:"courses",
     component:MainContentComponent
+  },
+  {
+    path:"profile",
+    component:ProfileSettingsComponent
   }
 ];
