f8575a6ae1c3550f29fcffe1ea036c5cb2518549
heldersantiago273 <heldersantiago273@gmail.com>
2024-09-06 18:29:02 +0100

    :sparkles: add routes for courses and home with new welcome compononet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, MainContentComponent, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    SidebarComponent,
+    MainContentComponent,
+    HeaderComponent,
+    RouterLink,
+    CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'students-dashboard';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainContentComponent } from './main-content/main-content.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component:WelcomePageComponent
+  },
+  {
+    path:"courses",
+    component:MainContentComponent
+  }
+];
