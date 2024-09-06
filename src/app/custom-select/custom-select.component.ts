15f27b0a84cf067be960c0d437fa5ebd10fb8bd2
heldersantiago273 <heldersantiago273@gmail.com>
2024-09-07 00:52:21 +0100

    :rocket: pushing to remote

---
+++ src/app/custom-select/custom-select.component.ts
@@ -0,0 +1,31 @@
+import { CommonModule } from '@angular/common';
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-select',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './custom-select.component.html',
+  styleUrls: ['./custom-select.component.scss'],
+})
+export class CustomSelectComponent {
+  public selectedOption: string = 'C2 - Advanced';
+  public options: string[] = [
+    'C2 - Avançado',
+    'B1 - Intermediário',
+    'A2 - Iniciante',
+  ];
+  public isOpen: boolean = false;
+
+  @Output() selectionChange = new EventEmitter<string>();
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.isOpen = false;
+    this.selectionChange.emit(option); // Emit selected value
+  }
+}
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CustomSelectComponent } from '../custom-select/custom-select.component';
 
 @Component({
   selector: 'app-welcome-page',
   standalone: true,
-  imports: [],
+  imports: [CustomSelectComponent],
   templateUrl: './welcome-page.component.html',
-  styleUrl: './welcome-page.component.scss'
+  styleUrl: './welcome-page.component.scss',
 })
 export class WelcomePageComponent {
+  selectedValue: string = '';
 
+  handleSelection(event: string) {
+    this.selectedValue = event;
+  }
 }
