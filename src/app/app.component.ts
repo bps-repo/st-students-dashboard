064fced1d782544c62890c75c567d5485d7457f9
heldersantiago273 <heldersantiago273@gmail.com>
2024-09-12 19:42:21 +0100

    :art: Add new folders Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 import { MainContentComponent } from './main-content/main-content.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
