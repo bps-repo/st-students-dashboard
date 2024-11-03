f68ac45b615bed1a4ae5941e307e2f77556615a3
heldersantiago273 <heldersantiago273@gmail.com>
2024-11-03 19:51:31 +0100

    :memo: event feature

--- src/app/features/events/events.component.ts
+++ src/app/features/events/events.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { TuiPopup } from '@taiga-ui/core';
+import { TuiBadge, TuiDrawer, TuiTabs } from '@taiga-ui/kit';
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TuiDrawer, TuiPopup, TuiBadge, TuiTabs],
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EventsComponent {
   events: any[] = [1, 2, 3, 4, 5];
+  protected readonly open = signal(false);
 }
