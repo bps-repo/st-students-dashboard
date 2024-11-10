d94bfadb82f6dbe10aba82f0c54df979503031f3
heldersantiago273 <heldersantiago273@gmail.com>
2024-11-10 22:31:29 +0100

    :sparkles: add lessons carousel

--- src/app/features/lessons/lessons.component.ts
+++ src/app/features/lessons/lessons.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { TuiButton } from '@taiga-ui/core';
+import { TuiCarousel, TuiPagination } from '@taiga-ui/kit';
 @Component({
   selector: 'app-lessons',
   standalone: true,
-  imports: [],
+  imports: [TuiCarousel, TuiPagination, CommonModule, TuiButton],
   templateUrl: './lessons.component.html',
-  styleUrl: './lessons.component.scss'
+  styleUrl: './lessons.component.scss',
 })
 export class LessonsComponent {
-
+  protected duration = 10_000;
+  protected index = 0;
+  protected items = Array.from({ length: 10 }, (_, i) => ({
+    label: `Lesson ${i + 1}`,
+    image: '/courses/course_blue.png',
+    color: 'primary',
+  }));
 }
