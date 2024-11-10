1f770db940e724391a26a795b8742a198773ce5c
heldersantiago273 <heldersantiago273@gmail.com>
2024-11-10 23:31:44 +0100

    :sparkles: finish event cards

--- src/app/features/events/events.component.ts
+++ src/app/features/events/events.component.ts
@@ -1,27 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { TuiPopup } from '@taiga-ui/core';
-import { TuiBadge, TuiDrawer, TuiTabs } from '@taiga-ui/kit';
+import { TuiBadge, TuiDrawer } from '@taiga-ui/kit';
 interface event {
   color: string;
   title?: string;
+  image?: string;
 }
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [CommonModule, TuiDrawer, TuiPopup, TuiBadge, TuiTabs],
+  imports: [CommonModule, TuiDrawer, TuiPopup, TuiBadge],
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EventsComponent {
   events: event[] = [
-    { color: 'primary' },
-    { color: 'success' },
-    { color: 'accent' },
-    { color: 'success' },
-    { color: 'primary' },
-    { color: 'success' },
+    { color: 'primary', image: '/users/1.png' },
+    { color: 'success', image: '/users/2.png' },
+    { color: 'accent', image: '/users/3.png' },
+    { color: 'success', image: '/users/2.png' },
+    { color: 'primary', image: '/users/1.png' },
+    { color: 'success', image: '/users/3.png' },
   ];
   protected readonly open = signal(false);
 }
