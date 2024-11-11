e9a8ad33e7bdd00f4122de6194e1d26b48ac757c
heldersantiago273 <heldersantiago273@gmail.com>
2024-11-11 15:43:56 +0100

    :sparkles: add support, timetables and tests components

---
+++ src/app/features/timetables/timetables.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetables',
+  standalone: true,
+  imports: [],
+  templateUrl: './timetables.component.html',
+  styleUrl: './timetables.component.scss'
+})
+export class TimetablesComponent {
+
+}
