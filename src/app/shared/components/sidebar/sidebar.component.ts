15bfb39c2a83287e966fccd1c5a4b4a88ace7b4d
heldersantiago273 <heldersantiago273@gmail.com>
2024-12-12 16:58:49 +0100

    :sparkles: Add navLink selected function

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,58 +1,89 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { BehaviorSubject } from 'rxjs';
 
+interface navLink {
+  label: string;
+  path: string;
+  icon: string;
+  activeIcon?: string;
+  active: boolean;
+}
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
-  private sidebarState = new BehaviorSubject<boolean>(true);
-  showSidebar$ = this.sidebarState.asObservable();
+export class SidebarComponent implements OnInit {
+  isNavBarOpened = signal(false);
 
-  navLinks = [
+  navLinks: navLink[] = [
     {
       label: 'Início',
       path: '/',
       icon: 'pi pi-th-large',
+      activeIcon: 'pi pi-th-large-alt',
+      active: false,
     },
     {
       label: 'Cursos',
       path: '/courses',
       icon: 'pi pi-tablet',
+      activeIcon: 'pi pi-tablet-alt',
+      active: false,
     },
     {
       label: 'Minhas Aulas',
       path: '/lessons',
       icon: 'pi pi-folder-open',
+      activeIcon: 'pi pi-folder-open-alt',
+      active: false,
     },
     {
       label: 'Professores',
       path: '/teachers',
       icon: 'pi pi-users',
+      activeIcon: 'pi pi-users-alt',
+      active: false,
     },
     {
       label: 'Eventos',
       path: '/events',
       icon: 'pi pi-calendar',
+      activeIcon: 'pi pi-calendar-alt',
+      active: false,
     },
     {
       label: 'Certificados',
       path: '/certificates',
       icon: 'pi pi-graduation-cap',
+      activeIcon: 'pi pi-graduation-cap-alt',
+      active: false,
     },
     {
       label: 'Ajuda',
       path: '/support',
       icon: 'pi pi-question-circle',
+      activeIcon: 'pi pi-question-circle-alt',
+      active: false,
     },
   ];
+
+  ngOnInit(): void {
+    this.navLinks[0].active = true;
+  }
+
   toggleSidebar() {
-    const currentState = this.sidebarState.getValue(); // Get current value
-    this.sidebarState.next(!currentState); // Toggle value
+    this.isNavBarOpened.set(!this.isNavBarOpened());
+  }
+
+  getSideBarIcon() {
+    return this.isNavBarOpened() ? 'pi pi-chevron-right' : 'pi pi-chevron-left';
+  }
+
+  setActiveTab(tab: navLink) {
+    this.navLinks.forEach((link) => (link.active = false));
+    tab.active = true;
   }
 }
