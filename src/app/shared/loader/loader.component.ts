13f5d04179dd9ed3a9c29812e6d3285aba79a2e1
heldersantiago273 <heldersantiago273@gmail.com>
2025-05-14 10:24:44 +0100

    Add loading indicators for student, level, and unit states in home page component

--- src/app/shared/loader/loader.component.ts
+++ src/app/shared/loader/loader.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Observable, of} from "rxjs";
+import {AsyncPipe, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-loader',
-  imports: [],
+  imports: [
+    NgIf,
+    AsyncPipe
+  ],
   templateUrl: './loader.component.html',
 })
 export class LoaderComponent {
+  @Input()
+  loading: Observable<boolean> = of(false)
+
+  @Input()
+  label: string = "Carregando..."
 }
